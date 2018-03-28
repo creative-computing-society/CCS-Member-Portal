7788d233d1615be8ec7a656e0e2939ea831ea5c4
Draguve <ritwikpatil1998@gmail.com>
2018-03-28 16:50:03 +0530

    Added flask boiler plate

---
+++ app.py
@@ -0,0 +1,43 @@
+import sqlite3 ,os
+from flask import Flask, flash, redirect, render_template, request, session, abort , g , url_for , jsonify
+from passlib.hash import sha256_crypt as sha
+ 
+app = Flask(__name__, static_url_path="", static_folder="static") #sets static folder which tells the url_for() in the html files where to look
+ 
+Database = 'ccslog.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(Database)
+    return db
+
+def query_db(query, args=(), one=False): #used to retrive values from the table
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+def execute_db(query , args=()): #executes a sql command like alter table and insert
+    conn = get_db()
+    cur = conn.cursor()
+    cur.execute(query , args)
+    conn.commit()
+    cur.close()
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+@app.route('/')
+def home():
+    return "Hello World"
+
+if __name__ == "__main__":
+    app.secret_key = os.urandom(12)
+    app.run(host = "0.0.0.0",debug=True, port=8080)
+
+
+
