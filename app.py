55807a494c8d0945726c491bc943014f94929c2e
Ramit <rkwap2011@gmail.com>
2018-04-21 18:29:02 +0530

    Added Add projects function

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ if app.config["DEBUG"]:
 
 """ app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
+app.config["SESSION_TYPE"] = "filesystem" 
 Session(app) """
 
 def login_required(f):
@@ -99,7 +99,7 @@ def signup():
             return "Wrong password"
 
         if query_db("select username from users where username = ?", (submission["username"],))!=[]:
-            error = "Username already taken"
+            error = "Username already taken" 
 
         password = sha.encrypt(submission["pass"])
         execute_db("insert into users values(?,?,?,?,?,0)", (
@@ -115,7 +115,7 @@ def signup():
 @login_required
 def profile():
     row=query_db('select * from users')
-    return render_template('member.html', un=session["username"], row=row)
+    return render_template('member2.html', un=session["username"], row=row)
 
 @app.route('/events')
 @login_required
@@ -138,6 +138,29 @@ def projects():
     row=query_db('select * from projects')
     return render_template('projects.html', un=session["username"], row=row)
 
+
+@app.route('/addprojects', methods=['GET', 'POST'])
+def addproject():
+    if request.method == "GET":
+        return render_template("projects.html")
+    else:
+        submission = {}
+        submission["title"] = request.form["title"]
+        submission["content"] = request.form["content"]
+        submission["images"] = request.form["images"]
+
+        if query_db("select title from projects where title = ?", (submission["title"],))!=[]:
+            error = "Project Title already exists! Please change the title." 
+        else:
+            execute_db("insert into projects values(?,?,?,0)", (
+                submission["title"],
+                submission["content"],
+                submission["images"],
+            ))
+        return redirect(url_for("projects"))
+
+
+
 @app.route("/logout")
 def logout():
     session.clear()
@@ -155,13 +178,13 @@ def change():
             submission = {}
             submission["pass"] = request.form["password"]
             submission["conf_pass"] = request.form["conf_pass"]
-
+            
             if submission["pass"]!=submission["conf_pass"]:
                 flash("Password doesnt match")
                 return redirect(url_for("change"))
-
+            
             password = sha.encrypt(submission["pass"])
-
+            
             execute_db("update users set password = ? where username = ?", (
             password,
             session["username"],))
